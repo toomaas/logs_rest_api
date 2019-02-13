e7d00580bbf1a8c500f1d06934948c0c1d9d076a
Joaquim Abreu <joaquim.abreu@ams.com>
2019-02-13 12:31:39 +0000

    logs.js added writestream for debugging added ecosystem.config.js with pm2 (temporary: dont start node with npm)

--- routes/api/logs/logs.js
+++ routes/api/logs/logs.js
@@ -10,17 +10,19 @@ module.exports.post = async function (req, res) {
     },
     maxContentLength: 31457280    //maxlength 30 mb
   };
-
+var stream = fs.createWriteStream("axios_error.txt",{flags:'a'});
   try {
-    console.log("json size: " + data.length);
     let result = await axios.post(url, data, config);
-    res.send(result.data);
+    res.send("ok");
+    stream.write(new Date().toISOString()+result+" \n");
   } catch (error) {
-    fs.writeFile("aixos_error.txt", error, (err) => {
-      // throws an error, you could also catch it here
-      if (err) throw err;
-      // success case, the file was saved
-      console.log('file saved!');
-    });
+    res.send("error");
+    stream.write("error \n");
+    // fs.writeFile("aixos_error.txt", error, (err) => {
+    //   // throws an error, you could also catch it here
+    //   if (err) throw err;
+    //   // success case, the file was saved
+    //   console.log('file saved!');
+    // });
   }
 };
