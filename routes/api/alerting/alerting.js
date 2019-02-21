4cbf99e506416e22bb72faccdccc9cb9e926cd3f
Joaquim Abreu <joaquim.abreu@ams.com>
2019-02-21 18:14:14 +0000

    making an insert if not exists query

--- routes/api/alerting/alerting.js
+++ routes/api/alerting/alerting.js
@@ -31,13 +31,27 @@ module.exports.get = async function (req, res) {
 module.exports.post = async function (req, res) {
     try {
         let validate_email = await email_validator.validate(req.body.email)
-        if (validate_email) {
+        if (validate_email && typeof req.body.app_code !== 'undefined' && req.body.app_code !== '') {
             // valid email
+            const connection = await oracle_conn()
+            let insert_query = await connection.execute(`INSERT INTO JALD_ALERTING_EMAILS (EMAIL, APP_CODE) SELECT '${req.body.email}', '${req.body.app_code}' FROM dual WHERE NOT EXISTS (SELECT NULL FROM JALD_ALERTING_EMAILS WHERE EMAIL = '${req.body.email}' AND APP_CODE = '${req.body.app_code}')`)
 
 
 
+
+
+            //             MERGE INTO JALD_ALERTING_EMAILS  d
+// USING (SELECT 'joaquim.abreu@ams.com' EMAIL , 'LDSI' APP_CODE  from dual) s
+// ON (d.EMAIL = s.EMAIL AND d.APP_CODE = s.APP_CODE)
+// WHEN NOT MATCHED THEN INSERT (EMAIL, APP_CODE) VALUES (s.EMAIL, s.APP_CODE)
+
+
+
+
+
+            // let insert_query = await connection.execute(`INSERT INTO JALD_ALERTING_EMAILS (APP_CODE,EMAIL) VALUES ('${req.body.app_code}','')`)
         } else {
-            res.send('not a valid email')
+            res.send('not a valid input')
         }
         // const connection = await oracledb.getConnection({
         //     user: process.env.USER,
